9a82fea6d008b8f2783c855c77ba6801a0bbc223
Atsushi Koshikizawa <koshikizawa@ipl.cs.uec.ac.jp>
2015-12-07 16:49:26 +0900

    ファイル名を変更

--- redcarpet_sample.rb
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env ruby
-require 'redcarpet'
-
-markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-
-input_file = File.open(ARGV[0], 'r') do |io|
-  puts markdown.render(io.read)
-end
