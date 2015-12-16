15bd70e89ffa2784ea82c85807aef41b0c4380d1
Atsushi KOSHIKIZAWA <koshikizawa@ipl.cs.uec.ac.jp>
2015-12-16 18:33:36 +0900

    定義リストの正規表現のバグが治った

--- lib/definition_block.rb
+++ lib/definition_block.rb
@@ -1,7 +1,7 @@
 # Definition List
 
 def definition_block(document, deflist_symbol)
-  document.gsub! /^([\s\S]+?)\n#{deflist_symbol}\ ([\s\S]+?)\n/ do
+  document.gsub! /^(.+?)\n#{deflist_symbol}\ ([\s\S]+?)\n/ do
     "<dl>\n  <dt>#{$1}</dt>\n  <dd>#{$2}</dd>\n</dl>\n"
   end
 end
