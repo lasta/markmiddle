34b4a46eb89ddc54c68f24f714f14dcb7724f07d
Atsushi KOSHIKIZAWA <koshikizawa@ipl.cs.uec.ac.jp>
2015-12-16 17:50:42 +0900

    execution_blockが存在するときに他に何もしなくなるバグを修正した
    その結果、definition_blockの正規表現が腐っていることが判明した
    
    次:
    * definition_blockの正規表現の修

--- lib/execution_block.rb
+++ lib/execution_block.rb
@@ -5,25 +5,54 @@ require 'open3'
 
 def execution_block(document, tag)
   tag_open, tag_close = tag
-  if document =~ /^#{tag_open}(.+)\n([\s\S]+?)#{tag_close}/ then
-    # out : 標準出力と標準エラーの両方が入る
-    # Open3.#caputure3 を用いると、stdout, stderr に分割できる
-    out, status = nil
-    # execするスクリプト本体 source_file
-    # source_file = Tempfile.new('', 'r', 0777)
-    source_file = Tempfile.new('')
+  document.gsub! /^#{tag_open}([\s\S]+?)\n([\s\S]+?)#{tag_close}/ do
+    exec($1, $2)
+  end
+end
+
+def exec(command, source)
+  # out : 標準出力と標準エラーの両方が入る
+  # Open3.#caputure3 を用いると、stdout, stderr に分割できる
+  out, status = nil
+  # execするスクリプト本体 source_file
+  # source_file = Tempfile.new('', 'r', 0777)
+  source_file = Tempfile.new('')
+  begin
+    source_file.write source
+    source_file.rewind # ポインタを先頭に
     begin
-      source_file.write $2
-      source_file.rewind # ポインタを先頭に
-      begin
-        out, status = Open3.capture2e($1 + " " + source_file.path)
-      rescue
-        puts "execution_block error"
-      end
-    ensure
-      source_file.close
-      source_file.unlink
+      out, status = Open3.capture2e(command + " " + source_file.path)
+    rescue
+      puts "execution_block error"
     end
+  ensure
+    source_file.close
+    source_file.unlink
   end
   return out
 end
+
+# def execution_block(document, tag)
+#   tag_open, tag_close = tag
+#   if document =~ /^#{tag_open}(.+)\n([\s\S]+?)#{tag_close}/ then
+#     # out : 標準出力と標準エラーの両方が入る
+#     # Open3.#caputure3 を用いると、stdout, stderr に分割できる
+#     out, status = nil
+#     # execするスクリプト本体 source_file
+#     # source_file = Tempfile.new('', 'r', 0777)
+#     source_file = Tempfile.new('')
+#     begin
+#       source_file.write $2
+#       source_file.rewind # ポインタを先頭に
+#       begin
+#         out, status = Open3.capture2e($1 + " " + source_file.path)
+#       rescue
+#         puts "execution_block error"
+#       end
+#     ensure
+#       source_file.close
+#       source_file.unlink
+#     end
+#   end
+#   return out
+# end
